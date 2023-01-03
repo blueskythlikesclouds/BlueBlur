6e35ec9bc5fdb38c2fedad4a742a53c64806cc26
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2023-01-03 18:46:00 +0300

    Instance info visibility flag.

--- Hedgehog/MirageCore/Misc/hhInstanceInfo.h
+++ Hedgehog/MirageCore/Misc/hhInstanceInfo.h
@@ -9,10 +9,17 @@ namespace Hedgehog::Animation
 
 namespace Hedgehog::Mirage
 {
+    enum EInstanceInfoFlags
+    {
+        eInstanceInfoFlags_Invisible = 0x1
+    };
+
     class CInstanceInfo : public CMatrixNodeListener
     {
     public:
-        BB_INSERT_PADDING(0x24C);
+        BB_INSERT_PADDING(0x7C);
+        uint8_t m_Flags; // see EInstanceInfoFlags
+        BB_INSERT_PADDING(0x1CF);
         Math::CMatrix m_Transform;
         Math::CMatrix m_PrevTransform;
         BB_INSERT_PADDING(0x20);
@@ -20,6 +27,7 @@ namespace Hedgehog::Mirage
         BB_INSERT_PADDING(0x48);
     };
 
+    BB_ASSERT_OFFSETOF(CInstanceInfo, m_Flags, 0x80);
     BB_ASSERT_OFFSETOF(CInstanceInfo, m_Transform, 0x250);
     BB_ASSERT_OFFSETOF(CInstanceInfo, m_PrevTransform, 0x290);
     BB_ASSERT_OFFSETOF(CInstanceInfo, m_spAnimationPose, 0x2F0);
