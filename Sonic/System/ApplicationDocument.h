1bf7d378153af6fcecbd44f6d7f71d6dc0d0eae6
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2023-01-07 15:02:50 +0300

    More application document mapping.

--- Sonic/System/ApplicationDocument.h
+++ Sonic/System/ApplicationDocument.h
@@ -9,8 +9,15 @@ namespace Hedgehog::Database
     class CDatabaseLoader;
 }
 
+namespace Hedgehog::Mirage
+{
+    class CRenderingInfrastructure;
+}
+
 namespace Sonic
 {
+    class CParameterEditor;
+
     class CApplicationDocument : public Hedgehog::Base::CSynchronizedObject
     {
     public:
@@ -19,9 +26,13 @@ namespace Sonic
         public:
             BB_INSERT_PADDING(0x1C);
             Hedgehog::Universe::CMessageManager* m_pMessageManager;
-            BB_INSERT_PADDING(0xA4);
+            BB_INSERT_PADDING(0x18);
+            boost::shared_ptr<Hedgehog::Mirage::CRenderingInfrastructure> m_spRenderingInfrastructure;
+            BB_INSERT_PADDING(0x84);
             boost::shared_ptr<Hedgehog::Database::CDatabaseLoader> m_spDatabaseLoader;
-            BB_INSERT_PADDING(0x160);
+            BB_INSERT_PADDING(0x134);
+            boost::shared_ptr<CParameterEditor> m_spParameterEditor;
+            BB_INSERT_PADDING(0x24);
 
             virtual ~CMember() = default;
         };
@@ -43,7 +54,9 @@ namespace Sonic
     };
 
     BB_ASSERT_OFFSETOF(CApplicationDocument::CMember, m_pMessageManager, 0x20);
+    BB_ASSERT_OFFSETOF(CApplicationDocument::CMember, m_spRenderingInfrastructure, 0x3C);
     BB_ASSERT_OFFSETOF(CApplicationDocument::CMember, m_spDatabaseLoader, 0xC8);
+    BB_ASSERT_OFFSETOF(CApplicationDocument::CMember, m_spParameterEditor, 0x204);
     BB_ASSERT_SIZEOF(CApplicationDocument::CMember, 0x230);
 
     BB_ASSERT_OFFSETOF(CApplicationDocument, m_pMember, 0x4);
