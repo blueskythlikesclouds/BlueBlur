156aeab3ee768708bc25d5716be0b45a7dee30cc
Ahremic <96267117+Ahremic@users.noreply.github.com>
2023-03-29 12:52:22 -0500

    Fleshed out GameDocument mapping
    Most virtual classes should be mapped out at this point, not counting anything nested.

--- Sonic/System/GameDocument.h
+++ Sonic/System/GameDocument.h
@@ -13,6 +13,11 @@ namespace Hedgehog::Database
 {
     class CDatabase;
 }
+namespace Hedgehog::Mirage
+{
+    class CDebugDraw;
+    class CLightFieldManager;
+}
 
 namespace Sonic
 {
@@ -21,6 +26,20 @@ namespace Sonic
     class CLightManager;
     class CRenderDirector;
     class CWorld;
+    class CParticleManager;
+    class CParameterBank;
+    class CNextStagePreloadingManager;
+    class CEventManager;
+
+    // TODO: Move elsewhere & rename
+    class BGMData
+    {
+        boost::shared_ptr<void> m_spSoundHandleBGM;
+        Hedgehog::Base::CSharedString m_SongName;
+        BB_INSERT_PADDING(0x08);
+        float m_PlaybackSpeed;
+    };
+    BB_ASSERT_SIZEOF(BGMData, 0x18);
 
     static inline BB_FUNCTION_PTR(void, __stdcall, fpAddGameObject, 0xD631A0,
         CGameDocument* pGameDocument, const Hedgehog::Base::THolder<CWorld>& worldHolder,
@@ -38,12 +57,26 @@ namespace Sonic
             hh::map<Hedgehog::Base::CSharedString, boost::shared_ptr<CWorld>> m_Worlds;
             boost::shared_ptr<Hedgehog::Database::CDatabase> m_spDatabase;
             hh::list<boost::shared_ptr<CGameObject>> m_GameObjects;
-            BB_INSERT_PADDING(0x18);
+            hh::vector<void*> m_UnkVector01;
+            boost::shared_ptr<Hedgehog::Mirage::CDebugDraw> m_spDebugDraw;
             hh::vector<size_t> m_PlayerIDs;
-            BB_INSERT_PADDING(0x28);
+            BB_INSERT_PADDING(0x20);
+            boost::shared_ptr<Hedgehog::Mirage::CLightFieldManager> m_spLightFieldManager;
             boost::shared_ptr<CLightManager> m_spLightManager;
             Hedgehog::Base::CSharedString m_StageName;
-            BB_INSERT_PADDING(0x124);
+            hh::vector<BGMData> m_AudioData;
+            float m_Field9C;
+            BB_INSERT_PADDING(0x08);
+            Hedgehog::Base::CSharedString m_Name01;
+        	BB_INSERT_PADDING(0x48);
+            CParameterBank* m_pParamaterBank;
+            BB_INSERT_PADDING(0x28);
+            boost::shared_ptr<CNextStagePreloadingManager> m_spNextStagePreloadingManager;
+            BB_INSERT_PADDING(0x04);
+            boost::shared_ptr<CParticleManager> m_spParticleManager;
+            CParticleManager* m_pParticleManager; // Odd.
+            boost::shared_ptr<CEventManager> m_spEventManager;
+            BB_INSERT_PADDING(0x70);
         };
 
         static constexpr Hedgehog::Base::TSynchronizedPtr<CGameDocument>* ms_pInstance = (Hedgehog::Base::TSynchronizedPtr<CGameDocument>*)0x1E0BE5C;
@@ -54,7 +87,9 @@ namespace Sonic
         }
 
         CMember* m_pMember;
-        BB_INSERT_PADDING(0x14);
+        BB_INSERT_PADDING(0x0C);
+        void* m_pGameActParameter;
+        BB_INSERT_PADDING(0x04);
 
         virtual ~CGameDocument() = default;
 
@@ -83,6 +118,14 @@ namespace Sonic
     BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_PlayerIDs, 0x48);
     BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_spLightManager, 0x80);
     BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_StageName, 0x88);
+    BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_AudioData, 0x8C);
+    BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_Field9C, 0x9C);
+    BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_Name01, 0xA8);
+    BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_pParamaterBank, 0xF4);
+    BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_spNextStagePreloadingManager, 0x120);
+    BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_spParticleManager, 0x12C);
+    BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_pParticleManager, 0x134);
+    BB_ASSERT_OFFSETOF(CGameDocument::CMember, m_spEventManager, 0x138);
     BB_ASSERT_SIZEOF(CGameDocument::CMember, 0x1B0);
 
     BB_ASSERT_OFFSETOF(CGameDocument, m_pCriticalSection, 0x4);
