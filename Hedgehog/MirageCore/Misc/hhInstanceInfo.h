7d8b19709052b060bdac2d967b75e5e037a19a10
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2023-04-12 01:41:29 +0300

    CPose fixup in CInstanceInfo.

--- Hedgehog/MirageCore/Misc/hhInstanceInfo.h
+++ Hedgehog/MirageCore/Misc/hhInstanceInfo.h
@@ -2,13 +2,10 @@
 
 #include <Hedgehog/MirageCore/MatrixNode/hhMatrixNodeListener.h>
 
-namespace Hedgehog::Animation
-{
-    class CAnimationPose;
-}
-
 namespace Hedgehog::Mirage
 {
+    class CPose;
+
     enum EInstanceInfoFlags
     {
         eInstanceInfoFlags_Invisible = 0x1
@@ -23,13 +20,13 @@ namespace Hedgehog::Mirage
         Math::CMatrix m_Transform;
         Math::CMatrix m_PrevTransform;
         BB_INSERT_PADDING(0x20);
-        boost::shared_ptr<Animation::CAnimationPose> m_spAnimationPose;
+        boost::shared_ptr<CPose> m_spPose;
         BB_INSERT_PADDING(0x48);
     };
 
     BB_ASSERT_OFFSETOF(CInstanceInfo, m_Flags, 0x80);
     BB_ASSERT_OFFSETOF(CInstanceInfo, m_Transform, 0x250);
     BB_ASSERT_OFFSETOF(CInstanceInfo, m_PrevTransform, 0x290);
-    BB_ASSERT_OFFSETOF(CInstanceInfo, m_spAnimationPose, 0x2F0);
+    BB_ASSERT_OFFSETOF(CInstanceInfo, m_spPose, 0x2F0);
     BB_ASSERT_SIZEOF(CInstanceInfo, 0x340);
 }
