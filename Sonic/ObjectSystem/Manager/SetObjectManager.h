3003386ecebe62f85ddc8ec0a9a1e4703969c636
Lorenzo Raineri <lorenzorai05@gmail.com>
2024-06-10 00:20:27 +0200

    Added OFFSETOF assert for SetObjectManager variables

--- Sonic/ObjectSystem/Manager/SetObjectManager.h
+++ Sonic/ObjectSystem/Manager/SetObjectManager.h
@@ -19,10 +19,13 @@ namespace Sonic
             boost::shared_ptr<CSetObjectEventManager> m_spSetObjectEventManager;
         };
 
+        BB_ASSERT_OFFSETOF(CSetObjectManagerMember, m_spUserIDGroupCategoryManager, 0x38);
+        BB_ASSERT_OFFSETOF(CSetObjectManagerMember, m_spParameterBankManager, 0x40);
         BB_ASSERT_OFFSETOF(CSetObjectManagerMember, m_spSetLayerManager, 0x50);
+        BB_ASSERT_OFFSETOF(CSetObjectManagerMember, m_spSetObjectEventManager, 0x58);
+
         BB_INSERT_PADDING(0xAC);
         CSetObjectManagerMember* m_pMember;
     };
-
     BB_ASSERT_OFFSETOF(CSetObjectManager, m_pMember, 0xAC);
 }
