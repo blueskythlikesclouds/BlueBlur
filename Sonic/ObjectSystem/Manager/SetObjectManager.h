50e6717d35bee11c359b16be348eeb8e3aec545a
RadiantDerg <jayvier13@gmail.com>
2024-06-19 07:21:10 -0700

    Update SetObjectManager.h
    Added CSetObjectManager inherited classes, removed padding

--- Sonic/ObjectSystem/Manager/SetObjectManager.h
+++ Sonic/ObjectSystem/Manager/SetObjectManager.h
@@ -1,17 +1,22 @@
 #pragma once
+#include <Hedgehog/Base/Thread/hhSynchronizedObject.h>
+#include <Sonic/System/GameObject.h>
+
 namespace Sonic
 {
     class CUserIDGroupCategoryManager;
     class CParameterBankManager;
     class CSetLayerManager;
     class CSetObjectEventManager;
-    class CSetObjectManager
+
+    class CSetObjectManager : public Sonic::CGameObject, public Hedgehog::Base::CSynchronizedObject
     {
     public:
         class CMember
         {
         public:
             BB_INSERT_PADDING(0x30);
+            //boost::shared_ptr<CSetObjectFactory> m_spSetObjectFactory;
             boost::shared_ptr<CUserIDGroupCategoryManager> m_spUserIDGroupCategoryManager;
             boost::shared_ptr<CParameterBankManager> m_spParameterBankManager;
             BB_INSERT_PADDING(0x10);
@@ -19,9 +24,9 @@ namespace Sonic
             boost::shared_ptr<CSetObjectEventManager> m_spSetObjectEventManager;
         };
 
-        BB_INSERT_PADDING(0xAC);
         CMember* m_pMember;
     };
+
     BB_ASSERT_OFFSETOF(CSetObjectManager, m_pMember, 0xAC);
     BB_ASSERT_OFFSETOF(CSetObjectManager::CMember, m_spUserIDGroupCategoryManager, 0x30);
     BB_ASSERT_OFFSETOF(CSetObjectManager::CMember, m_spParameterBankManager, 0x38);
