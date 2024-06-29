d77513d1f3c9dc1aa2f610e41cca25aeabe60268
Lorenzo Raineri <lorenzorai05@gmail.com>
2024-06-29 19:24:21 +0200

    Renamed SetObjectListener functions + SetObjectManager progress

--- Sonic/ObjectSystem/Manager/SetObjectManager.h
+++ Sonic/ObjectSystem/Manager/SetObjectManager.h
@@ -15,19 +15,19 @@ namespace Sonic
         class CMember
         {
         public:
-            BB_INSERT_PADDING(0x30);
-            //boost::shared_ptr<CSetObjectFactory> m_spSetObjectFactory;
+            BB_INSERT_PADDING(0x10);
+            boost::shared_ptr<Sonic::CSetObjectFactory> m_spSetObjectFactory;
+            BB_INSERT_PADDING(0x18);
             boost::shared_ptr<CUserIDGroupCategoryManager> m_spUserIDGroupCategoryManager;
             boost::shared_ptr<CParameterBankManager> m_spParameterBankManager;
             BB_INSERT_PADDING(0x10);
             boost::shared_ptr<CSetLayerManager> m_spSetLayerManager;
             boost::shared_ptr<CSetObjectEventManager> m_spSetObjectEventManager;
         };
-
         CMember* m_pMember;
     };
-
     BB_ASSERT_OFFSETOF(CSetObjectManager, m_pMember, 0xAC);
+    BB_ASSERT_OFFSETOF(CSetObjectManager::CMember, m_spSetObjectFactory, 0x10);
     BB_ASSERT_OFFSETOF(CSetObjectManager::CMember, m_spUserIDGroupCategoryManager, 0x30);
     BB_ASSERT_OFFSETOF(CSetObjectManager::CMember, m_spParameterBankManager, 0x38);
     BB_ASSERT_OFFSETOF(CSetObjectManager::CMember, m_spSetLayerManager, 0x50);
