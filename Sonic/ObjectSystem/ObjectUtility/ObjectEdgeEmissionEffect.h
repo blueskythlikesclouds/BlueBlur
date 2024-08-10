34937c29231673f342c914a273b53e7bc3ade516
brianuuu <38166666+brianuuu@users.noreply.github.com>
2024-08-10 14:58:32 +0100

    Space no tabs aaaaaaaaaaa

--- Sonic/ObjectSystem/ObjectUtility/ObjectEdgeEmissionEffect.h
+++ Sonic/ObjectSystem/ObjectUtility/ObjectEdgeEmissionEffect.h
@@ -4,25 +4,25 @@
 
 namespace Sonic
 {
-	class CObjectEdgeEmissionEffect : public Hedgehog::Mirage::CSingleElementEffect
-	{
-	public:
-		static constexpr size_t ms_pVtbl = 0x16CFB8C;
+    class CObjectEdgeEmissionEffect : public Hedgehog::Mirage::CSingleElementEffect
+    {
+    public:
+        static constexpr size_t ms_pVtbl = 0x16CFB8C;
 
-		float m_EdgeEmissionParam;
-		float m_RateOfChange;
-		float m_CurrentValue;
-		float m_MaxValue;
-	};
+        float m_EdgeEmissionParam;
+        float m_RateOfChange;
+        float m_CurrentValue;
+        float m_MaxValue;
+    };
 
-	CObjectEdgeEmissionEffect();
-	CObjectEdgeEmissionEffect(float in_RateOfChange);
+    CObjectEdgeEmissionEffect();
+    CObjectEdgeEmissionEffect(float in_RateOfChange);
 
-	BB_ASSERT_OFFSETOF(CObjectEdgeEmissionEffect, m_EdgeEmissionParam, 0x40);
-	BB_ASSERT_OFFSETOF(CObjectEdgeEmissionEffect, m_RateOfChange, 0x44);
-	BB_ASSERT_OFFSETOF(CObjectEdgeEmissionEffect, m_CurrentValue, 0x48);
-	BB_ASSERT_OFFSETOF(CObjectEdgeEmissionEffect, m_MaxValue, 0x4C);
-	BB_ASSERT_SIZEOF(CObjectEdgeEmissionEffect, 0x50);
+    BB_ASSERT_OFFSETOF(CObjectEdgeEmissionEffect, m_EdgeEmissionParam, 0x40);
+    BB_ASSERT_OFFSETOF(CObjectEdgeEmissionEffect, m_RateOfChange, 0x44);
+    BB_ASSERT_OFFSETOF(CObjectEdgeEmissionEffect, m_CurrentValue, 0x48);
+    BB_ASSERT_OFFSETOF(CObjectEdgeEmissionEffect, m_MaxValue, 0x4C);
+    BB_ASSERT_SIZEOF(CObjectEdgeEmissionEffect, 0x50);
 }
 
 #include <Sonic/ObjectSystem/ObjectUtility/CObjectEdgeEmissionEffect.inl>
