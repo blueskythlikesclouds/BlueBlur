28599267e5e0d7bd19b110bfe982e78b4e9a6dd7
NextinHKRY <38560522+NextinMono@users.noreply.github.com>
2024-10-17 20:29:58 +0200

    Add MainSequence, SequenceMode and ModeCreaterList (#18)
    * Added MainSequence and SequenceMode classes
    
    * Updated name of CSequenceMode04
    
    * Added includes and fixed nesting
    
    * Update SequenceMain.h

---
+++ Sonic/Sequence/SequenceMain.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Hedgehog/Base/Type/hhSharedString.h>
+#include <Hedgehog/Universe/Engine/hhMessageActor.h>
+
+namespace Sonic::Sequence
+{
+	class CStoryImpl;
+	class CSequenceMainImpl : public Hedgehog::Universe::CMessageActor
+	{
+	public:
+		Hedgehog::Base::CSharedString m_CurrentSequenceModeName;
+		boost::shared_ptr<CSequenceMode> m_spCurrentSequenceMode;
+		BB_INSERT_PADDING(0xC);
+		CStoryImpl* m_pStorySequence;
+		BB_INSERT_PADDING(0x8);
+	};
+	BB_ASSERT_OFFSETOF(CSequenceMainImpl, m_CurrentSequenceModeName, 0x7C);
+	BB_ASSERT_OFFSETOF(CSequenceMainImpl, m_spCurrentSequenceMode, 0x80);
+	BB_ASSERT_OFFSETOF(CSequenceMainImpl, m_pStorySequence, 0x94);
+}
