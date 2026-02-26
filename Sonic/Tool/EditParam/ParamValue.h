c354f9142e86ff7671da9cd973b9feed9b24ee12
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-02-26 13:43:33 +0000

    Remove assertions from Sonic::CParamValue

--- Sonic/Tool/EditParam/ParamValue.h
+++ Sonic/Tool/EditParam/ParamValue.h
@@ -19,15 +19,5 @@ namespace Sonic
 
         FuncData* m_pFuncData;
         T m_DefaultValue;
-
-        BB_ASSERT_OFFSETOF(FuncData, m_pValue, 0x8);
-        BB_ASSERT_OFFSETOF(FuncData, m_Value, 0xC);
-        BB_ASSERT_OFFSETOF(FuncData, m_ChangedCallback, 0x10);
-        BB_ASSERT_OFFSETOF(FuncData, m_Field30, 0x30);
-        BB_ASSERT_SIZEOF(FuncData, 0x38);
-
-        BB_ASSERT_OFFSETOF(CParamValue, m_pFuncData, 0x14);
-        BB_ASSERT_OFFSETOF(CParamValue, m_DefaultValue, 0x18);
-        BB_ASSERT_SIZEOF(CParamValue, 0x1C);
     };
 }
