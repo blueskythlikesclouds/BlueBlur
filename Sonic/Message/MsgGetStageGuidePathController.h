d53559e8ef59492b3bd434052fe770f1901d0c53
NextinHKRY <38560522+NextinMono@users.noreply.github.com>
2026-06-12 20:17:11 +0200

    Add Stage related messages
    Co-Authored-By: RadiantDerg <9061202+RadiantDerg@users.noreply.github.com>

---
+++ Sonic/Message/MsgGetStageGuidePathController.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <Hedgehog/Universe/Engine/hhMessage.h>
+
+namespace Sonic::Message
+{
+	class MsgGetStageGuidePathController : public Hedgehog::Universe::MessageTypeGet
+	{
+	public:
+		HH_FND_MSG_MAKE_TYPE(0x015C7B4C);
+		boost::anonymous_shared_ptr m_spStageGuidePath;
+	};
+	BB_ASSERT_OFFSETOF(MsgGetStageGuidePathController, m_spStageGuidePath, 0x10);
+	BB_ASSERT_SIZEOF(MsgGetStageGuidePathController, 0x18);
+}
